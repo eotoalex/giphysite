3c50ff07f7a40f5a07a9b802dc4851d4e9f56069
Alex Puello <apuello88@yahoo.com>
2019-10-07 18:04:44 -0400

    added the button generator and am now working on the api button interface.

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,79 @@
+var apiKey = "&api_key=FE0K9W1wRMIZlBbC9i3hekFL1bNmkHZk";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ button + apiKey;
+var topics = ["anime", "horror", "drama", "romance", "action"]
+
+// Generate button elements.
+function buttonGen (){
+   
+   for (var i = 0; i < topics.length; i++){
+    var createButton = $("<button>");
+    createButton.addClass("topic-buttons");
+    createButton.attr("data",topics[i]);
+    createButton.attr("value", i);
+    createButton.text(topics[i]);
+    $("body").append(createButton);
+
+   }
+
+};
+$.ajax({
+    url:queryURL,
+    method:"GET"
+    }).then(function(response){
+    console.log(response);
+    })
+
+    // var button = $(buttonGen).attr("data");
+    var x = response;
+    $("#text-input").append(x);
+
+
+
+// Create click event for the buttons.
+
+$(".topic-buttons").on("click", function(){
+    var userclick = $(this).val()
+    var elementData = $(this).attr("data");
+    // $.ajax({
+    //     url:queryURL,
+    //     method:"GET"
+    //     }).then(function(response){
+    //     console.log(response);
+    //     })
+    
+        // var generateGifs = $("<img>");
+        // generateGifs.addClass(elementData,"-gifs");
+        // generateGifs.attr("value", "gif-",i);
+        // generateGifs.attr("src", response.data);
+        // $("#text-input").append(generateGifs);
+
+
+  
+
+
+
+})
+
+
+
+// Create a function that checks user text input. Remember to use .trim and param() method to alter ajax request to get the appropriate data.
+
+
+
+
+
+// Functions to be called.
+
+buttonGen();
+
+
+
+
+// Gif generater function.
+
+// Evaluation of input field.
+
+// user input object generator
+
+// user input modified for ajax call.
+
